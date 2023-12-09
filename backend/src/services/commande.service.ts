fb82a3aeb82062fe1f19e9d74eaa3c146e560036
ThibaultVercoutre <vercoutre.thibault@gmail.com>
2023-12-09 18:25:31 +0100

    problème résolu

---
+++ backend/src/services/commande.service.ts
@@ -0,0 +1,31 @@
+// import { connexion } from "../../bdd/connect.js";
+// import { Plat } from "../models/plat.model.js";
+
+// export async function getCommandeById(id: number) {
+//     try{
+//         const db = await connexion();
+//         console.log("db", db);
+
+//         return new Promise((resolve, reject) => {
+//             const sql = `SELECT p.name as platname, t.name as typename, p.price as price, r.date as date FROM reservation r 
+//                         INNER JOIN commande c ON r.id  = c.id_reservation
+//                         INNER JOIN plat p ON p.id = c.id_plat
+//                         INNER JOIN type t ON t.id = p.id_type
+//                         WHERE r.id = ?
+//                         ORDER BY t.id`;
+//             db.all(sql, [id], (err, rows) => {
+//                 if (err) {
+//                     db.close();
+//                     reject(err);
+//                 } else {
+//                     const users = rows.map(row => new Plat(row));
+//                     db.close();
+//                     resolve(users);
+//                 }
+//             });
+//         });
+//     } catch (err) {
+//         console.error(err);
+//         throw err;
+//     }
+// }
